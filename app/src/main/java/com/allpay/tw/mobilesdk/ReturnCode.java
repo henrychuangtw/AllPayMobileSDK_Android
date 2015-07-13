eb163811e06ab0046badffe27b5c73fe997640ba
lucas <lucas@rolymobile.com>
2015-07-13 16:58:20 +0800

    added return code

---
+++ app/src/main/java/com/allpay/tw/mobilesdk/ReturnCode.java
@@ -0,0 +1,9 @@
+package com.allpay.tw.mobilesdk;
+
+public interface ReturnCode {
+    int CREDIT_CARD_PAY_SUCCESS = 1;
+    int ATM_TAKE_NUMBER_SUCCESS = 2;
+    int CREDIT_CARD_OTP_VERIFICATION = 3;
+    int CONVENIENCE_STORE_TAKE_NUMBER_SUCCESS = 10100073;
+    int VERIFICATION_CODE_ERROR = 10000030;
+}
