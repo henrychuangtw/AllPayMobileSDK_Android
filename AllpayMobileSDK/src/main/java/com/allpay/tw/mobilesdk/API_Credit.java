83277e766a34364a0e571f1281c7d1879fa06ff6
lucas <lucas@rolymobile.com>
2016-06-30 17:52:51 +0800

    added API_Credit member var

--- AllpayMobileSDK/src/main/java/com/allpay/tw/mobilesdk/API_Credit.java
+++ AllpayMobileSDK/src/main/java/com/allpay/tw/mobilesdk/API_Credit.java
@@ -11,4 +11,18 @@ public class API_Credit extends API_Base {
 	public String TradeDate;
 	public String SimulatePaid;
 	public String OtpExpiredTime;
+	public String card4no;
+	public String card6no;
+	public String stast;
+	public String eci;
+	public String red_dan;
+	public String red_de_amt;
+	public String staed;
+	public String amount;
+	public String process_date;
+	public String gwsr;
+	public String auth_code;
+	public String red_yet;
+	public String OtpResult;
+	public String red_ok_amt;
 }
